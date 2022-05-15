1516e5a08c263dd2d0bc111ae231dc9c26367f6a
Filipe Moura <filipeasm18@gmail.com>
2022-05-15 20:57:24 -0300

    implement dynamic run

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from grafo import Grafo
 
 grafo = Grafo()
-grafo.caminhoMinimo("toy.txt", 0, 4)
+arquivo = input("Informe o nome do arquivo presente em /dataset: ")
+origem = int(input("Informe o vértice de origem: "))
+destino = int(input("Informe o vértice de destino: "))
+print("\nProcessando...")
+
+grafo.caminhoMinimo(arquivo, origem, destino)
