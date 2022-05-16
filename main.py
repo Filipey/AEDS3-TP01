e707ed2fb0acb9b3301911080003ba0941001118
Filipe Moura <filipeasm18@gmail.com>
2022-05-16 11:12:54 -0300

    insert timestamp

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from grafo import Grafo
+import time
 
 grafo = Grafo()
 arquivo = input("Informe o nome do arquivo presente em /dataset: ")
 origem = int(input("Informe o vértice de origem: "))
 destino = int(input("Informe o vértice de destino: "))
+inicio = time.process_time()
 print("\nProcessando...")
 
 grafo.caminhoMinimo(arquivo, origem, destino)
+fim = time.process_time()
+print(f"Tempo: {fim - inicio}s")
